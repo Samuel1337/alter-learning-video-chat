93ff7760facf8a0edbd161b481507bac392a32ab
Samuel Martins <smbra33@gmail.com>
2023-05-08 22:55:35 -0700

    set up Github Pages

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={window.location.pathname || ''}>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
