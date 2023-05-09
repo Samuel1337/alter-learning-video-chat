4922545800e02e228782044f58a5e095721ee6a9
Samuel Martins <smbra33@gmail.com>
2023-05-09 12:08:37 -0700

    add token

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,24 @@ import { Navigator } from '@src/routes/Navigator';
 import React, { useMemo } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, useLocation } from 'react-router-dom';
+import VoxeetSDK from '@voxeet/voxeet-web-sdk';
 
 import './App.module.scss';
 
 const App = () => {
   const location = useLocation();
-
+  VoxeetSDK.packageUrlPrefix = `${window.location.origin}${import.meta.env.BASE_URL}assets/wasm`
   const urlToken = useMemo(() => {
     return encodeURIComponent(new URLSearchParams(window.location.search).get('token') || '');
   }, [location]);
 
-  const YOUR_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkb2xieS5pbyIsImlhdCI6MTY4MzYwMjUyMywic3ViIjoiemQzREt5amlsY3B2bUd2WU1UZXg5dz09IiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DVVNUT01FUiJdLCJ0YXJnZXQiOiJzZXNzaW9uIiwib2lkIjoiZWM3NjFkYWYtYzk0OS00MGEwLTg4NzQtYjczMTg5MDRmYjE2IiwiYWlkIjoiZjI5ZmI5YmItNDY2My00YjY4LWJjZjgtODcxODIwNDg0ZThjIiwiYmlkIjoiOGEzNjlkNDM4N2ZiM2Q1YzAxODdmYzk0MjQwNzY0MmIiLCJleHAiOjE2ODM2ODg5MjN9.M6NqrQ6b8sz3IRcr64pn2Bv0z_OkycclYHYF0TNigNrXbK56yw2BC3-Pe0OthD5mLbkJhSepFG_1Bz9_3m_icw";
+  const YOUR_TOKEN = urlToken;
 
   return (
     <TranslationProvider>
       <ConferenceCreateProvider>
         <CommsProvider
-          token={YOUR_TOKEN} 
+          token={"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkb2xieS5pbyIsImlhdCI6MTY4MzYwMjUyMywic3ViIjoiemQzREt5amlsY3B2bUd2WU1UZXg5dz09IiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DVVNUT01FUiJdLCJ0YXJnZXQiOiJzZXNzaW9uIiwib2lkIjoiZWM3NjFkYWYtYzk0OS00MGEwLTg4NzQtYjczMTg5MDRmYjE2IiwiYWlkIjoiZjI5ZmI5YmItNDY2My00YjY4LWJjZjgtODcxODIwNDg0ZThjIiwiYmlkIjoiOGEzNjlkNDM4N2ZiM2Q1YzAxODdmYzk0MjQwNzY0MmIiLCJleHAiOjE2ODM2ODg5MjN9.M6NqrQ6b8sz3IRcr64pn2Bv0z_OkycclYHYF0TNigNrXbK56yw2BC3-Pe0OthD5mLbkJhSepFG_1Bz9_3m_icw"} 
           packageUrlPrefix={`${window.location.origin}${
             import.meta.env.BASE_URL
           }assets/wasm`}
